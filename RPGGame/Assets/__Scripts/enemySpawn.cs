fe31d639de7540b4b33d5d56e695ca05bb8b3d8c
luxscious <57682940+luxscious@users.noreply.github.com>
2021-02-23 23:40:30 -0500

    inital commit

---
+++ RPGGame/Assets/__Scripts/enemySpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawn : MonoBehaviour
+{
+
+    public GameObject rat;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i=0; i<2; i++)
+        {         
+        Instantiate (rat, new Vector3(Random.Range(8.0f, 15.0f), 0f,-1.0f), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+
+
