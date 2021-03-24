50392e05565d4d09b066c0d5a75000ef2ac099e2
emachej <emachej@uwo.ca>
2021-03-24 05:33:22 -0400

    Added enemies
    Added 2 new types of enemies to the game.
    Added unique movement for each different type of enemy. Enemies move at different speeds and at different directions and elevations.

---
+++ RPGGame/Assets/__Scripts/_enemies/ghost_move.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ghost_move : MonoBehaviour
+{
+
+    Vector2 pointA;
+    Vector2 pointB;
+    
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointA = new Vector2(transform.position.x,4);
+        pointB = new Vector2(transform.position.x+3,4);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        
+        move();
+    }
+
+
+
+
+    void move()
+    {
+        transform.position = Vector2.Lerp(pointA, pointB, Mathf.PingPong(Time.time*1f, 1));
+    }
+}
