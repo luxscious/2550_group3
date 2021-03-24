d1a2cd006623905ccd752d6a535c80dfba726b5d
luxscious <57682940+luxscious@users.noreply.github.com>
2021-03-24 04:48:54 -0400

    Revert "Merge branch 'main' into Phase2-Karis"
    This reverts commit fa6de1044f2b5f901cdab5401fb8102e37b918f3, reversing
    changes made to 7d979059eba7a6d942c66688ab81135787ea976b.

---
+++ RPGGame/Assets/__Scripts/Interactable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour 
+{
+    public bool withinRange;
+    public KeyCode interactKey;
+    public UnityEvent interactAction;
+
+    void Update()
+    {
+        if (withinRange) //if in the range to interact
+        {
+            if (Input.GetKeyDown(interactKey))
+            {
+                interactAction.Invoke();
+            }
+        }
+    }
+
+    
+    public virtual void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            withinRange = true;
+            Debug.Log("Player now in range");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            withinRange = false;
+            Debug.Log("Player now out of range");
+        }
+    }
+    
+}
+
---
+++ RPGGame/Assets/__Scripts/ItemPickup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : Interactable
+{
+    public Item item;
+
+    public override void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            withinRange = true;
+            PickUp();
+        }
+    }
+
+    void PickUp()
+    {
+        Debug.Log("Picking up " + item.name);
+        bool wasPickedUp = Inventory.instance.Add(item); //Add to inventory
+
+        if(wasPickedUp)
+            Destroy(gameObject);
+    }
+
+}
